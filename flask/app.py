1bd817f3c1f6de5e67b7c250ab1d883b269a2b73
rudrakpatra <rudrakpatracodes@gmail.com>
2023-04-30 21:21:42 +0530

    asd

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
  
+@app.route('/')
+def home():
+    return '''
+<html>
+  <body>
+    <form action="http://localhost:5000/login" method="post">
+      <p>Enter Name:</p>
+      <p><input type="text" name="nm" /></p>
+      <p><input type="submit" value="submit" /></p>
+    </form>
+  </body>
+</html>
+'''
  
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
+@app.route('/read/<filename>')
+def read(filename):
+    return 'reading %s' % filename
  
  
 @app.route('/login', methods=['POST', 'GET'])
